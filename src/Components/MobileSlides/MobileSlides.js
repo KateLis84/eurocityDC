598372acdaf796180d06661efe8a783b4c571627
KateLis84 <emma12022002@gmail.com>
2023-05-08 17:25:40 +0300

    add test swiper for mobile

--- src/Components/MobileSlides/MobileSlides.js
+++ src/Components/MobileSlides/MobileSlides.js
@@ -1,273 +1,189 @@
-import {React, useEffect} from 'react';
-import { Swiper, SwiperSlide} from 'swiper/react';
-import { EffectCoverflow, Mousewheel, Pagination, Navigation } from 'swiper';
-import $ from 'jquery';
-import 'swiper/css';
-import 'swiper/css/effect-coverflow';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-import './MobileSlides.scss';
-
-
-
+import { React, useEffect } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectCoverflow, Mousewheel, Pagination, Navigation } from "swiper";
+import $ from "jquery";
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "./MobileSlides.scss";
 
 export default function MobileSlide() {
 
-  var bg = document.querySelector('.item-bg');
-  var items = document.querySelectorAll('.news__item');
-  var item = document.querySelector('.news__item');
-
-  function cLog(content) {
-      console.log(content)
-  }
-
-function smallScreen(){
-    $(document).on("mouseover", ".news__item", function (_event, _element) {
-
-        var newsItem = document.querySelectorAll('.news__item');
-        newsItem.forEach(function (element, index) {
-            element.addEventListener('mouseover', function () {
-                var x = this.getBoundingClientRect().left;
-                var y = this.getBoundingClientRect().top;
-                var width = this.getBoundingClientRect().width;
-                var height = this.getBoundingClientRect().height;
-
-                $('.item-bg').addClass('active');
-                $('.news__item').removeClass('active');
-                // $('.news__item').removeClass('active');
-
-                bg = document.querySelector('.item-bg');
-                bg.style.width = width + 'px';
-                bg.style.height = height + 'px';
-                bg.style.transform = 'translateX(' + x + 'px ) translateY(' + y + 'px)';
-            });
-
-            element.addEventListener('mouseleave', function () {
-                $('.item-bg').removeClass('active');
-                $('.news__item').removeClass('active');
-            });
-
-        });
-
-    });
-}
-
-useEffect(()=>{
-  bg=document.querySelector('.item-bg');
-  if($(window).width() > 800) smallScreen();
-}, [])
-
-
-function touchEnd () {
-    $('.news__item').removeClass('active');
-    $('.swiper-slide-active .news__item').addClass('active');
-};
-
-function onSlideChange () {
-    $('.news__item').removeClass('active');
-};
-
-function slideChangeTransitionEnd () {
-    $('.news__item').removeClass('active');
-    var activeItem = document.querySelector('.swiper-slide-active');
-
-    var sliderItem = activeItem.querySelector('.news__item');
-
-    $('.swiper-slide-active .news__item').addClass('active');
-
-    var x = sliderItem.getBoundingClientRect().left;
-    var y = sliderItem.getBoundingClientRect().top;
-    var width = sliderItem.getBoundingClientRect().width;
-    var height = sliderItem.getBoundingClientRect().height;
-
-
-    $('.item-bg').addClass('active');
-    bg=document.querySelector('.item-bg');
-    bg.style.width = width + 'px';
-    bg.style.height = height + 'px';
-    bg.style.transform = 'translateX(' + x + 'px ) translateY(' + y + 'px)';
-};
+//   if(window.innerWidth < 1200) {
+//     new Swiper(".swiper-container",{
+//         direction: "horizontal",
+//         slidesPerView: 1,
+//         nextButton: ".swiper-button-next",
+//         prevButton: ".swiper-button-prev",
+//         paginationClickable: !0,
+//         spaceBetween: 0,
+//         autoplay: 2500,
+//         loop: !0
+//     })
+// } else {
+//     new Swiper(".swiper-container",{
+//         direction: "horizontal",
+//         slidesPerView: 1,
+//         parallax: !0,
+//         nextButton: ".swiper-button-next",
+//         prevButton: ".swiper-button-prev",
+//         paginationClickable: !0,
+//         spaceBetween: 0,
+//         speed: 1500,
+//         parallax: !0,
+//         autoplay: 2500,
+//         loop: !0
+//     })
+// }
 
 
   return (
-    <>
-    <div class="item-bg"></div>
-
-      <Swiper className="news-slider"
-        modules={[EffectCoverflow, Pagination, Navigation]}
-        effect="coverflow"
-        grabCursor={true}
-        loop={true}
-        centeredSlides={true}
-        spaceBetween= "0"
-        slidesPerView= 'auto'
-        speed="300"
-        coverflowEffect={{
-          rotate: 0,
-          stretch: 0,
-          depth: 0,
-          modifier: 3,
-          slideShadows: false
-        }}
-        breakpoints={{
-          480: {
-              spaceBetween: 0,
-              centeredSlides: true
-          }
-        }}
-        simulateTouch={true}
-        navigation={{
-            nextEl: '.news-slider-next',
-            prevEl: '.news-slider-prev'
-        }}
-        pagination={{
-            el: '.news-slider__pagination',
-            clickable: true
-        }}
-        onSlideChange={()=>onSlideChange()}
-        onSlideChangeTransitionEnd={()=>slideChangeTransitionEnd()}
-        onTouchEnd={()=>{touchEnd()}}
-      >
-          <div className="news-slider__wrp swiper-wrapper">
-            <SwiperSlide className="news-slider__item swiper-slide">
-              <a href="#" className="news__item">
-                <div className="news-date">
-                  <span className="news-date__title">24</span>
-                  <span className="news-date__txt">May</span>
+    <section class="section__slider">
+      <div class="container__center">
+        <Swiper class="swiper-container"
+          direction="horizontal"
+          slidesPerView="1"
+          paginationClickable="!0"
+          spaceBetween={0}
+          speed={1500}
+          autoplay={2500}
+          loop={true}
+        >
+          <div class="swiper-wrapper">
+            <SwiperSlide class="swiper-slide">
+              <div class="swiper-slide__block">
+                <div
+                  class="swiper-slide__block__img"
+                  data-swiper-parallax-y="70%"
+                >
+                  <a target="_blank" href="#">
+                    <img
+                      src="https://images.unsplash.com/photo-1456518563096-0ff5ee08204e?auto=format&amp;fit=crop&amp;w=1351&amp;q=60&amp;ixid=dW5zcGxhc2guY29tOzs7Ozs%3D"
+                      alt=""
+                    />
+                  </a>
                 </div>
-                <div className="news__title">Lorem Ipsum Dolor Sit Amed</div>
-
-                <p className="news__txt">
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                  standard dummy text ever since the 1500s...
-                </p>
-
-                <div className="news__img">
-                  <img
-                    src="https://res.cloudinary.com/muhammederdem/image/upload/q_60/v1537132205/news-slider/item-2.webp"
-                    alt="news"
-                  />
+                <div class="swiper-slide__block__text">
+                  <h2 data-swiper-parallax-x="-60%" class="main__title">
+                    Slider
+                    <br />
+                    One
+                    <span>.</span>
+                  </h2>
+                  <h3 data-swiper-parallax-x="-50%" class="main__subtitle">
+                    Tagline
+                    <span> • 2017</span>
+                  </h3>
+                  <p data-swiper-parallax-x="-40%" class="paragraphe">
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text.
+                  </p>
+                  <a
+                    data-swiper-parallax-x="-30%"
+                    class="link"
+                    target="_blank"
+                    href="#"
+                  >
+                    Discover
+                  </a>
+                  <span data-swiper-parallax-y="60%" class="number">
+                    1
+                  </span>
                 </div>
-              </a>
+              </div>
             </SwiperSlide>
-
-            <SwiperSlide className="news-slider__item swiper-slide">
-              <a href="#" className="news__item">
-                <div className="news-date">
-                  <span className="news-date__title">25</span>
-                  <span className="news-date__txt">May</span>
+            <SwiperSlide class="swiper-slide">
+              <div class="swiper-slide__block">
+                <div
+                  class="swiper-slide__block__img"
+                  data-swiper-parallax-y="70%"
+                >
+                  <a target="_blank" href="#">
+                    <img
+                      src="https://images.unsplash.com/photo-1510709657750-f5a80fc8da9c?auto=format&amp;fit=crop&amp;w=1950&amp;q=60&amp;ixid=dW5zcGxhc2guY29tOzs7Ozs%3D"
+                      alt=""
+                    />
+                  </a>
                 </div>
-                <div className="news__title">Lorem Ipsum Dolor Sit Amed</div>
-
-                <p className="news__txt">
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                  standard dummy text ever since the 1500s...
-                </p>
-
-                <div className="news__img">
-                  <img
-                    src="https://res.cloudinary.com/muhammederdem/image/upload/q_60/v1537132205/news-slider/item-3.webp"
-                    alt="news"
-                  />
+                <div class="swiper-slide__block__text">
+                  <h2 data-swiper-parallax-x="-60%" class="main__title">
+                    Slider
+                    <br />
+                    Two
+                    <span>.</span>
+                  </h2>
+                  <h3 data-swiper-parallax-x="-50%" class="main__subtitle">
+                    Tagline
+                    <span>• 2017</span>
+                  </h3>
+                  <p data-swiper-parallax-x="-40%" class="paragraphe">
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text.
+                  </p>
+                  <a
+                    data-swiper-parallax-x="-30%"
+                    style={{zIndex: 5}}
+                    class="link"
+                    target="_blank"
+                    href="#"
+                  >
+                    Discover
+                  </a>
+                  <span data-swiper-parallax-y="60%" class="number">
+                    2
+                  </span>
                 </div>
-              </a>
+              </div>
             </SwiperSlide>
-
-            <SwiperSlide className="news-slider__item swiper-slide">
-              <a href="#" className="news__item">
-                <div className="news-date">
-                  <span className="news-date__title">26</span>
-                  <span className="news-date__txt">May</span>
+            <SwiperSlide class="swiper-slide">
+              <div class="swiper-slide__block">
+                <div
+                  class="swiper-slide__block__img"
+                  data-swiper-parallax-y="70%"
+                >
+                  <a target="_blank" href="#">
+                    <img
+                      src="https://images.unsplash.com/photo-1505567745926-ba89000d255a?auto=format&amp;fit=crop&amp;w=1951&amp;q=60&amp;ixid=dW5zcGxhc2guY29tOzs7Ozs%3D"
+                      alt=""
+                    />
+                  </a>
                 </div>
-                <div className="news__title">Lorem Ipsum Dolor Sit Amed</div>
-
-                <p className="news__txt">
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                  standard dummy text ever since the 1500s...
-                </p>
-
-                <div className="news__img">
-                  <img
-                    src="https://res.cloudinary.com/muhammederdem/image/upload/q_60/v1537132205/news-slider/item-4.webp"
-                    alt="news"
-                  />
+                <div class="swiper-slide__block__text">
+                  <h2 data-swiper-parallax-x="-60%" class="main__title">
+                    Slider
+                    <br />
+                    Three
+                    <span>.</span>
+                  </h2>
+                  <h3 data-swiper-parallax-x="-50%" class="main__subtitle">
+                    Tagline
+                    <span>• 2017</span>
+                  </h3>
+                  <p data-swiper-parallax-x="-40%" class="paragraphe">
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text.
+                  </p>
+                  <a
+                    data-swiper-parallax-x="-30%"
+                    class="link"
+                    target="_blank"
+                    href="#"
+                  >
+                    Discover
+                  </a>
+                  <span data-swiper-parallax-y="60%" class="number">
+                    3
+                  </span>
                 </div>
-              </a>
-            </SwiperSlide>
-
-            <SwiperSlide className="news-slider__item swiper-slide">
-              <a href="#" className="news__item">
-                <div className="news-date">
-                  <span className="news-date__title">27</span>
-                  <span className="news-date__txt">May</span>
-                </div>
-                <div className="news__title">Lorem Ipsum Dolor Sit Amed</div>
-
-                <p className="news__txt">
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                  standard dummy text ever since the 1500s...
-                </p>
-
-                <div className="news__img">
-                  <img
-                    src="https://res.cloudinary.com/muhammederdem/image/upload/q_60/v1537132205/news-slider/item-2.webp"
-                    alt="news"
-                  />
-                </div>
-              </a>
-            </SwiperSlide>
-
-            <SwiperSlide className="news-slider__item swiper-slide">
-              <a href="#" className="news__item">
-                <div className="news-date">
-                  <span className="news-date__title">28</span>
-                  <span className="news-date__txt">May</span>
-                </div>
-                <div className="news__title">Lorem Ipsum Dolor Sit Amed</div>
-
-                <p className="news__txt">
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                  standard dummy text ever since the 1500s...
-                </p>
-
-                <div className="news__img">
-                  <img
-                    src="https://res.cloudinary.com/muhammederdem/image/upload/q_60/v1537132205/news-slider/item-5.webp"
-                    alt="news"
-                  />
-                </div>
-              </a>
-            </SwiperSlide>
-
-            <SwiperSlide className="news-slider__item swiper-slide">
-              <a href="#" className="news__item">
-                <div className="news-date">
-                  <span className="news-date__title">29</span>
-                  <span className="news-date__txt">May</span>
-                </div>
-                <div className="news__title">Lorem Ipsum Dolor Sit Amed</div>
-
-                <p className="news__txt">
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                  standard dummy text ever since the 1500s...
-                </p>
-
-                <div className="news__img">
-                  <img
-                    src="https://res.cloudinary.com/muhammederdem/image/upload/q_60/v1537132205/news-slider/item-4.webp"
-                    alt="news"
-                  />
-                </div>
-              </a>
+              </div>
             </SwiperSlide>
           </div>
-      </Swiper>
-    </>
+        </Swiper>
+      </div>
+    </section>
   );
-};
+}
