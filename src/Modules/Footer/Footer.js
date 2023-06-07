6186281c43534ea9b7621e7fb07eb3e71b544252
KateLis84 <emma12022002@gmail.com>
2023-06-07 22:28:20 +0300

    add footer interaction

--- src/Modules/Footer/Footer.js
+++ src/Modules/Footer/Footer.js
@@ -1,31 +1,60 @@
-import React from 'react';
+import * as React from 'react';
 import './Footer.scss'
+import { HashLink as Link } from 'react-router-hash-link';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
 function Footer() {
 
-  return(
+  const [open, setOpen] = React.useState(false);
+
+  const handleClick = (event) => {
+    let text = event.currentTarget.innerText;
+    navigator.clipboard.writeText(text);
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  return (
     <div className="footer">
       <div className="footer__element footer__element_contacts">
         <div className="footer__title">Контакти</div>
-        <div className="footer__text">+390 876 654  844</div>
-        <div className="footer__text">eurocitydevelopment@gmail.com</div>
+        <div className="footer__text" onClick={handleClick}>+390 876 654 844</div>
+        <div className="footer__text" onClick={handleClick}>eurocitydevelopment@gmail.com</div>
       </div>
-      <div className="footer__element footer__element_cities"> 
-        <div className="footer__title">Міста</div>
-        <div className='footer__cities'>
-          <div className="footer__text">Київ</div>
-          <div className="footer__text">Львів</div>
-          <div className="footer__text">Житомир</div>
-          <div className="footer__text">Чернівці</div>
+      <div className="footer__element footer__element_cities">
+        <div className="footer__title">Комплекси</div>
+        <div className="footer__cities">
+          <div className="footer__text">Стожари</div>
+          <div className="footer__text">Виyковий</div>
+          <div className="footer__text">Green Village</div>
         </div>
       </div>
       <div className="footer__element footer__element_phone">
         <div className="footer__title">Зв'язок</div>
-        <div className="footer__text">Зателефонуйте нам</div>
-        <div className="footer__text">Задайте нам питання</div>
+        <Link style={{ all: "unset" }} to="contacts">
+          <div className="footer__text">Зателефонуйте нам</div>
+        </Link>
+        <a style={{ all: "unset" }} href= "mailto:eurocitydevelopment@gmail.com"><div className="footer__text">Задайте нам питання</div></a>
       </div>
+      <Snackbar open={open} autoHideDuration={2000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+          Текст скопійовано!
+        </Alert>
+      </Snackbar>
     </div>
-  )
+  );
 }
 
 export default Footer;
